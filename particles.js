f66c47073a0b313d359e17cd6b38f8e829e2fd28
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2024-01-15 18:07:22 +0100

    Particle image example

--- particles.js
+++ particles.js
@@ -1,11 +1,12 @@
+import { Pane } from "tweakpane";
 import * as THREE from "three";
-
 export default class Particles {
 	constructor(renderer, scene, flowmap) {
 		this.renderer = renderer;
 		this.scene = new THREE.Scene();
 		this.flowmap = flowmap;
 		this.clock = new THREE.Clock();
+		this.particles = 228;
 
 		this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
@@ -15,23 +16,26 @@ export default class Particles {
 		this.textures = this.images.map((image) => {
 			this.loader.load(image);
 		});
+		this.texture = this.loader.load("./public/sand.jpg");
 
-		this.geometry = new THREE.PlaneGeometry(4, 3, 128, 128);
+		this.geometry = new THREE.PlaneGeometry(4, 3, this.particles, this.particles);
 		this.material = new THREE.ShaderMaterial({
 			uniforms: {
-				tMap: { value: this.textures[0] },
+				tMap: { value: this.texture },
 				tFlow: { value: this.flowmap },
 				uTime: { value: 0 },
 			},
 			vertexShader: /* glsl */ `
-                uniform sampler2D tMap;
+                uniform sampler2D tFlow;
                 varying vec2 vUv;
                 
                 void main() {
                     vUv = uv;
-                    vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );
+                    vec4 flow = texture2D(tFlow, uv);
+                    vec3 pos = position + flow.rgb * 0.1;
+                    vec4 mvPosition = modelViewMatrix * vec4( pos, 1.0 );
                     gl_Position = projectionMatrix * mvPosition;
-                    gl_PointSize = 2.0;
+                    gl_PointSize = 5.0;
                 }
             `,
 			fragmentShader: /* glsl */ `
@@ -52,11 +56,18 @@ export default class Particles {
 
 	addObject() {
 		this.mesh = new THREE.Points(this.geometry, this.material);
-		this.mesh.position.z = -1;
+		this.mesh.position.z = -3;
 		this.scene.add(this.mesh);
 	}
 
 	render() {
 		this.renderer.render(this.scene, this.camera);
+	}
+
+	initPane() {
+		const pane = new Pane();
+		pane.addBinding(this.particles, "value", { min: 0, max: 1000 }).on("change", () => {
+			this.geometry = new THREE.PlaneGeometry(4, 3, this.particles.value, this.particles.value);
+		});
 	}
 }
